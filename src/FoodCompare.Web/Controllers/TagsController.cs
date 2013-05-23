ea8a141c49140f5aca5e79f5564ede3081eae361
Eddie Groves <eddiegroves@gmail.com>
2013-05-23 10:14:15 +1000

    Adding tags HTML

---
+++ src/FoodCompare.Web/Controllers/TagsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodCompare.Web.Controllers
+{
+    public class TagsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
