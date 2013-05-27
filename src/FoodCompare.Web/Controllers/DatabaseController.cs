972e9acdb88b98b4c65fd942accab57b56b73e9e
Eddie Groves <eddiegroves@gmail.com>
2013-05-28 08:47:51 +1000

    Adding database utils page

---
+++ src/FoodCompare.Web/Controllers/DatabaseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodCompare.Web.Controllers
+{
+    public class DatabaseController : BaseController
+    {
+        public DatabaseController(IDbConnection db) : base(db)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
