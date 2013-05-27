6e0756037bebe27ebb8899d13d5367c8d40bc624
Eddie Groves <eddiegroves@gmail.com>
2013-05-28 09:17:04 +1000

    Adding database import and clear

--- src/FoodCompare.Web/Controllers/DatabaseController.cs
+++ src/FoodCompare.Web/Controllers/DatabaseController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FoodCompare.Web.Data;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -9,13 +10,18 @@ namespace FoodCompare.Web.Controllers
 {
     public class DatabaseController : BaseController
     {
-        public DatabaseController(IDbConnection db) : base(db)
-        {
-        }
+        public DatabaseController(IDbConnection db) : base(db) { }
 
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Clear()
+        {
+            Database.Create();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ src/FoodCompare.Web/Controllers/ImportController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using CsvHelper;
+using ServiceStack.OrmLite;
+using FoodCompare.Web.Models;
+
+namespace FoodCompare.Web.Controllers
+{
+    public class ImportController : BaseController
+    {
+        private readonly IDbConnection _db;
+
+        public ImportController(IDbConnection db) : base(db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Tags(HttpPostedFileBase file)
+        {
+            Import<Tag>(file);
+            return RedirectToAction("Index", "Tags");
+        }
+
+        public ActionResult Foods(HttpPostedFileBase file)
+        {
+            Import<Food>(file);
+            return RedirectToAction("Index", "Foods");
+        }
+
+        private void Import<T>(HttpPostedFileBase file) where T : class, new()
+        {
+            using (var reader = new StreamReader(file.InputStream))
+            {
+                new DbCsvImporter(_db).Read<T>(reader);
+            }
+        }
+    }
+
+    public class DbCsvImporter
+    {
+        private readonly IDbConnection _db;
+
+        public DbCsvImporter(IDbConnection db)
+        {
+            _db = db;
+        }
+
+        public void Read<T>(TextReader reader) where T : class, new()
+        {
+            using (var csv = new CsvReader(reader))
+            {
+                var records = csv.GetRecords<T>().ToList();
+                _db.InsertAll<T>(records);
+            }
+        }
+    }
+}
