a69ea3b60f05502221ad0ee82224d1e1bbc44ef8
Eddie Groves <eddiegroves@gmail.com>
2013-05-28 18:13:37 +1000

    Better recovery when db schema missing

--- src/FoodCompare.Web/Controllers/BaseController.cs
+++ src/FoodCompare.Web/Controllers/BaseController.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using System.Web.Mvc;
 using FoodCompare.Web.Models;
 using ServiceStack.OrmLite;
@@ -17,7 +18,14 @@ namespace FoodCompare.Web.Controllers
 
         public void PopulateNavigationViewData()
         {
-            ViewBag.Tags = _db.Select<Tag>();
+            try
+            {
+                ViewBag.Tags = _db.Select<Tag>();
+            }
+            catch
+            { 
+                // Empty catch for the time being
+            }
         }
     }
 }
